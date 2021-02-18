a5c81ace310ee69438622c9e2a2ef17df97057a5
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-18 13:49:09 -0600

    ¿Cómo pueden venir las peticiones?

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const router = express.Router();
 
+let app = express();
 
-var app = express();
+app.use(router);
 
-app.use('/', function (req, res) {
-  res.send('Hola');
+router.get('/message', function (req, res) {
+    res.send('Lista de mensajes');
 });
+
+router.post('/message', function (req, res) {
+  res.send('Mensaje añadido');
+});
+
+router.delete('/message', function (req, res) {
+  res.send('Mensaje eliminado');
+});
+
+// app.use('/', function (req, res) {
+//   res.send('Hola');
+// });
 
 app.listen(3000);
 console.log('La aplicación está escuchando en el http://localhost:3000');
