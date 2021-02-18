34b410323af33f6d30cfd1f36acd26cdf267572e
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-18 15:26:40 -0600

    Respuestas coherentes

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const response = require('./network/response');
+
 const router = express.Router();
 
 let app = express();
+
+/*  Abreviatura
+req = request
+res = response
+ */
 
 // Enviar mensaje en formato .json
 app.use(bodyParser.json());
@@ -15,14 +23,20 @@ router.get('/message', function (req, res) {
   res.header({
     "custom-header": "Nuestro valor personalizado",
   });
-  res.send('Lista de mensajes');
+  // res.send('Lista de mensajes');
+  response.success(req, res, 'Lista de mensajes' );
 });
 
 router.post('/message', function (req, res) {
   // Enviar mensaje en formato query
   console.log(req.query);
-  console.log(req.body);
-  res.send('Mensaje ' + req.body.text + ' añadido correctamente');
+  if (req.query.error === "ok") {
+    response.error(req, res, 'Error simulado', 400 ); 
+  } else {
+    // res.send('Mensaje ' + req.body.text + ' añadido correctamente');
+    response.success(req, res, 'Añadido correctamente', 201 );
+  }
+
 });
 
 router.delete('/message', function (req, res) {
