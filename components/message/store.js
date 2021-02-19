ec6d49d04c7ca6577c9c26fdb671b09e3a8a6693
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-19 09:37:43 -0600

    MongoDB: Almacenar y leer datos con mongoose

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,25 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
+
+const uri = 'mongodb+srv://user_chat:RgAzkuF0R27DJ94L@cluster0.tmnrd.mongodb.net/telegrom?retryWrites=true&w=majority';
+
+db.Promise = global.Promise;
+
+db.connect(uri, 
+  { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('[db] Conectada con éxito'))
+    .catch(err => console.error('[db]', err));
+
 
 function addMessage(message) {
-  list.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-function getMessage() {
-  return list;
+async function getMessage() {
+  // return list;
+  const messages = await Model.find();
+  return messages;
 }
 
 module.exports = {
